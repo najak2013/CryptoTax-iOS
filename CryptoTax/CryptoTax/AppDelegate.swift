e1a90e01eb8f2a51042c94d50e8c3e8efae34b78
Kevin, Jitae Kim <wlxo0401@gmail.com>
2022-04-29 18:28:07 +0900

    프레임워크 테스트
    프레임워크 테스트

--- CryptoTax/CryptoTax/AppDelegate.swift
+++ CryptoTax/CryptoTax/AppDelegate.swift
@@ -43,7 +43,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate, UNUser
             guard let token = token else {
                 return
             }
-            print("Token : ", token)
+            print("FIR Token : ", token)
         }
     }
 
--- CryptoTax/CryptoTax/ViewControllers/Home/HomeViewController.swift
+++ CryptoTax/CryptoTax/ViewControllers/Home/HomeViewController.swift
@@ -6,13 +6,28 @@
 //
 
 import UIKit
+import CryptoTaxLibs
 
 class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        print("안녕하세요.")
+        
+        let userInfo = [
+            "userCI" : "김지태",
+            "userAccessKey" : "",
+            "userSecretKey" : ""
+        ]
+        
         // Do any additional setup after loading the view.
+        CryptoTaxLibs.shared.getUserTaxInfo(userInfo: userInfo, CryptoHandler: { result in
+            print("반환된 세금은 총 \(result)입니다.")
+                    })
+
+        print("세금 정보 요청 완료 결과를 기다려주세요.")
+
     }
     
 
